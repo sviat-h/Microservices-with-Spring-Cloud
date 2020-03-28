2261f7ac89e0db2ace2cf44b1f3cacf4421b939d
sviatoslav <svyatoslav.grinik@gmail.com>
2020-03-28 03:05:34 +0100

    Revert "added final version of hw23 v.2.0."
    This reverts commit 2d8a84f7a5925916973f7c7650c03ecec8c6bfc9.

--- accountService/src/main/java/com/cursor/AccountsApplication.java
+++ accountService/src/main/java/com/cursor/AccountsApplication.java
@@ -2,12 +2,19 @@ package com.cursor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class AccountsApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountsApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- profileService/src/main/java/com/cursor/ProfilesApplication.java
+++ profileService/src/main/java/com/cursor/ProfilesApplication.java
@@ -2,14 +2,19 @@ package com.cursor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients(basePackages = {"com.cursor.feign", "com.cursor.controller"})
 public class ProfilesApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProfilesApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
